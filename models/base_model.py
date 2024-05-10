9d0ccbd49a52c22b67b3126e4b21eb2129130c6c
nmarizani <n.marizani@alustudent.com>
2024-05-10 12:41:24 +0000

    Base model file created and tested

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import datetime
+import uuid
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes BaseModel
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
+    def save(self):
+        """ 
+        Updates the public instance attribute with current date time
+        """   
+        self.updated_at = datetime.datetime.now()    
+
+    
+    def to_dict(self):
+        """
+        Converts instance attributes to dictionary
+        """
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
