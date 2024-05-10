bb3d3d24334c3814c9bc250e3f9a3e918d205a38
Branis333 <b.sumba@alustudent.com>
2024-05-10 03:36:30 -0700

    console

--- console.py
+++ console.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 import cmd
 
-#This is for exiting the console
-
 class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand class provides a command-line interface for interacting with the HBNB program.
+    """
+
     prompt = '(hbnb)'
+
     def do_quit(self, arg):
-        """ Quit command to exit the program"""
+        """
+        Quit command to exit the program.
+        Usage: quit
+        """
         return True
+
     do_EOF = do_quit
 
     def emptyline(self):
-        """Do nothing on empty input line"""
+        """
+        Do nothing on empty input line.
+        """
         pass
 
 if __name__ == '__main__':
+    """
+    Entry point of the script.
+    """
     HBNBCommand().cmdloop()
