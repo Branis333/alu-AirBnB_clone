fd3624e4c22ed3662ad7474a06173de9b9a0be17
nmarizani <n.marizani@alustudent.com>
2024-05-15 13:00:13 +0000

    Base model updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,5 @@
-#!/usr/bin/python3
-
 import datetime
 import uuid
-from models import storage
-import models
-
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -14,26 +9,28 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.datetime.strptime(value,
-                                                       "%Y-%m-%dT%H:%M:%S.%f")
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            storage.new(self)
 
     def __str__(self):
-
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        """
+        Returns string representation of BaseModel
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
 
     def save(self):
+        """ 
+        Updates the public instance attribute with current date time
+        """   
+        self.updated_at = datetime.datetime.now()  
 
-        self.updated_at = datetime.datetime.now()
-        storage.save()
-
+    
     def to_dict(self):
         """
         Converts instance attributes to dictionary
