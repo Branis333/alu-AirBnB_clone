cef4e3acceccf966490c6f045eabcad6588f3eee
nmarizani <n.marizani@alustudent.com>
2024-05-17 11:57:10 +0000

    Test file ran

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 # Import modules and classes to make them available when the package is imported
-from .base_model import BaseModel
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from models.base_model import BaseModel
 
 from models.engine.file_storage import FileStorage
 
