636210089bbfbb8e177ccfb2a4426d52bd3797b5
Branis333 <b.sumba@alustudent.com>
2024-05-17 14:43:29 +0200

    user update

--- models/user.py
+++ models/user.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-
+"""This module creates a User class"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class that inherits from BaseModel"""
+    """Class for managing user objects"""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize User instance"""
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email', '')
-        self.password = kwargs.get('password', '')
-        self.first_name = kwargs.get('first_name', '')
-        self.last_name = kwargs.get('last_name', '')
-
-    def __str__(self):
-        """String representation of User"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
